772e260d54b47dba24ab644880756b6fea3fb10c
Catherine Bluemke <catbluemke@gmail.com>
2014-06-26 14:28:02 -0400

    made changing brand images on first page, gridded out people section, added gradient to background of main page

--- site/js/script.js
+++ site/js/script.js
@@ -3,6 +3,18 @@
 $(document).ready(function() {
 	var height = $(window).height();
 	$(".blocks").height(height);
+
+	$("#slideshow > div:gt(0)").hide();
+
+	setInterval(function() { 
+  	$('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+	},  3000);
+
 });
 
 $(window).resize(function () {
