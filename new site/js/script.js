bd5a4bfb35f1c785cbaffc4de2f36179b40a973d
Catherine Bluemke <catbluemke@gmail.com>
2014-07-07 10:29:02 -0400

    making new site

---
+++ new site/js/script.js
@@ -0,0 +1,60 @@
+
+
+$(document).ready(function() {
+
+
+	var height = $(window).height();
+	$(".blocks").height(height);
+
+	$(".icon").on("click",function(){
+		var content = $(this).attr("id");
+		console.log(content);
+		$("#iframe").attr("src",content + ".html");
+		$(".windowtitle").html("<p>"+content+"</p>");
+	});
+			var toggleON = {
+			height: height-50,
+			position: "absolute",
+			top: "50px",
+			left: "0px",
+			width: "50%",
+		};
+
+		var toggleOFF = {
+			height: "80%",
+			width:"80%",
+			position: "relative",
+			top: "0px",
+			margin:"0px",
+		};
+
+	$(".people").toggle(function(){
+
+		$(".people").not(this).hide();
+		$(this).children("img").css(toggleON);
+		$(this).children(".people_info").css("display","block");
+		},
+		function() {
+			$(this).children("img").css(toggleOFF);
+			$(this).children(".people_info").css("display","none");
+			$(".people").not(this).show();
+			
+
+		});
+	$("#twitbut").toggle(function(){
+		$("#twitter").show("slow");
+	},
+	function(){
+		$("#twitter").hide("slow");
+	})
+	
+});
+
+
+$(window).resize(function () {
+	var height = $(window).height();
+	$(".blocks").height(height);
+});
+
+
+
---
+++ site/js/script1.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function() {
+
+	var height = $(window).height();
+	$(".blocks").height(height);
+
+		var toggleON = {
+			height: height-50,
+			position: "absolute",
+			top: "50px",
+			left: "0px",
+			width: "50%",
+		};
+
+		var toggleOFF = {
+			height: "80%",
+			width:"80%",
+			position: "relative",
+			top: "0px",
+			margin:"0px",
+		};
+
+	$(".people").toggle(function(){
+
+		$(".people").not(this).hide();
+		$(this).children("img").css(toggleON);
+		$(this).children(".people_info").css("display","block");
+		},
+		function() {
+			$(this).children("img").css(toggleOFF);
+			$(this).children(".people_info").css("display","none");
+			$(".people").not(this).show();
+			
+
+		});
+	
+});
+
+$(window).resize(function () {
+	var height = $(window).height();
+	$(".blocks").height(height);
+});
+
+
